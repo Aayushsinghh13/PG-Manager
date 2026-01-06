389342464a057be3ef8a1a87fe12b2d1e41389d2
Vraj0Hub <vrajpatel.01810@gmail.com>
2026-01-06 10:43:19 +0530

    contributing.md and bug fixes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from psycopg2 import OperationalError
 import psycopg2
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, redirect, render_template, request, url_for
 
 bp = Blueprint("main", __name__)
 
@@ -8,6 +8,27 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # TODO: implement authentication
+        # email = request.form.get('email')
+        # password = request.form.get('password')
+        return redirect(url_for('main.index'))
+    return render_template('login.html')
+
+
+@bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # TODO: implement user creation and validation
+        # name = request.form.get('name')
+        # email = request.form.get('email')
+        # password = request.form.get('password')
+        return redirect(url_for('main.index'))
+    return render_template('signup.html')
 
 DB_CONFIG = {
     "dbname": "pgmanagement",
